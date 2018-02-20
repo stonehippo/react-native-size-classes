19c05371a94f9b4a3c554d0fdc46d382868b991d
Joe Wilson <joe@cantina.co>
2018-02-20 09:14:30 -0500

    Clean up export for Android

--- sizeclasses.js
+++ sizeclasses.js
@@ -4,11 +4,18 @@
 
 import { NativeModules, Platform } from 'react-native';
 
+let getSizeClasses, isIpad;
+
+// The RNSizeClasses module is only defined for iOS, so do not try to
+// load it on Android devices.
 if (Platform.OS === 'ios') {
   const RNSizeClasses = NativeModules.RNSizeClasses;
 
-  module.exports = {
-    getSizeClasses: RNSizeClasses.getSizeClasses,
-    isIpad: () => RNSizeClasses.isIpad,
-  }
+  getSizeClasses: RNSizeClasses.getSizeClasses;
+  isIpad = () => RNSizeClasses.isIpad;
+}
+
+module.exports = {
+  getSizeClasses,
+  isIpad,
 }
